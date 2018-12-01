cd7628144671ccc230da91b9890c0bb7ec706163
duskol <dulleo@gmail.com>
2018-12-02 00:22:36 +0100

    package name changed

--- src/main/java/com/duskol/ecdl/configuration/AppConfiguration.java
+++ src/main/java/com/duskol/ecdl/configuration/AppConfiguration.java
@@ -1,4 +1,4 @@
-package com.duskol.edcl.configuration;
+package com.duskol.ecdl.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
