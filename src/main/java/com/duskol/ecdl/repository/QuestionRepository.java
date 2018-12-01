29c00068064f27f235d3a37133d85279fd874596
duskol <dulleo@gmail.com>
2018-12-02 00:41:42 +0100

    package name changed

--- src/main/java/com/duskol/ecdl/repository/QuestionRepository.java
+++ src/main/java/com/duskol/ecdl/repository/QuestionRepository.java
@@ -1,11 +1,15 @@
-package com.duskol.edcl.repository;
+package com.duskol.ecdl.repository;
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.duskol.edcl.model.Question;
+import com.duskol.ecdl.model.Question;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long>{
+
+	List<Question> findByTestId(Long testId);
 
 }
