80dd79c0d922c812e6cbd74e1507f252f61dd0c3
duskol <dulleo@gmail.com>
2018-12-02 00:41:55 +0100

    package name changed

--- src/main/java/com/duskol/ecdl/controller/QuestionController.java
+++ src/main/java/com/duskol/ecdl/controller/QuestionController.java
@@ -1,4 +1,4 @@
-package com.duskol.edcl.controller;
+package com.duskol.ecdl.controller;
 
 import javax.validation.Valid;
 
@@ -6,17 +6,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.duskol.edcl.dto.QuestionDto;
-import com.duskol.edcl.service.QuestionService;
+import com.duskol.ecdl.controller.exception.ResourceNotFoundException;
+import com.duskol.ecdl.dto.QuestionDTO;
+import com.duskol.ecdl.model.Question;
+import com.duskol.ecdl.service.QuestionService;
 
 @RestController
-@RequestMapping(value="/ecdl/questions")
+@RequestMapping(value="/ecdl")
 public class QuestionController {
 	
 private final static Logger logger = LoggerFactory.getLogger(QuestionController.class);
@@ -24,11 +29,21 @@ private final static Logger logger = LoggerFactory.getLogger(QuestionController.
 	@Autowired
 	QuestionService questionService;
 	
-	@PostMapping(consumes = "application/json", produces = "application/json")
+	@PostMapping(path="/tests/{id}/questions",consumes = "application/json", produces = "application/json")
 	@ResponseStatus(value=HttpStatus.CREATED)
-	public QuestionDto createQuestion(@RequestBody QuestionDto questionDto) {
+	public QuestionDTO createQuestion(@RequestParam Long id,  @RequestBody @Valid QuestionDTO questionDto) throws ResourceNotFoundException {
 		logger.info("Save question:" + questionDto.toString());
-		return questionService.createQuestion(questionDto);
+		QuestionDTO createQuestion = questionService.createQuestion(id, questionDto);
+		logger.info("Created question:" + createQuestion.toString());
+		return createQuestion;
+	}
+	
+	@GetMapping("/{id}")
+	@ResponseStatus(value=HttpStatus.OK)
+	public Question getQuestion(@PathVariable Long id) {
+		
+		return questionService.getQuestion(id);
+		
 	}
 
 }
