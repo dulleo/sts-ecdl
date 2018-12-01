34444afa2606b393073a1c2b26c461b565d0fe48
duskol <dulleo@gmail.com>
2018-12-02 00:39:00 +0100

    package name changed

--- src/main/java/com/duskol/ecdl/repository/TestRepository.java
+++ src/main/java/com/duskol/ecdl/repository/TestRepository.java
@@ -1,9 +1,9 @@
-package com.duskol.edcl.repository;
+package com.duskol.ecdl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.duskol.edcl.model.Test;
+import com.duskol.ecdl.model.Test;
 
 @Repository
 public interface TestRepository extends JpaRepository<Test, Long>{
