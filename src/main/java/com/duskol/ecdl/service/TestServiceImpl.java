da9719c68da26710d6b3b393c6e0704483076700
duskol <dulleo@gmail.com>
2018-12-02 02:31:02 +0100

    delete test - delet questions -delete answers

--- src/main/java/com/duskol/ecdl/service/TestServiceImpl.java
+++ src/main/java/com/duskol/ecdl/service/TestServiceImpl.java
@@ -2,6 +2,9 @@ package com.duskol.ecdl.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,7 +12,11 @@ import org.springframework.stereotype.Service;
 import com.duskol.ecdl.controller.exception.ResourceNotFoundException;
 import com.duskol.ecdl.dto.TestDTO;
 import com.duskol.ecdl.error.ErrorCodes;
+import com.duskol.ecdl.model.Answer;
+import com.duskol.ecdl.model.Question;
 import com.duskol.ecdl.model.Test;
+import com.duskol.ecdl.repository.AnswerRepository;
+import com.duskol.ecdl.repository.QuestionRepository;
 import com.duskol.ecdl.repository.TestRepository;
 import com.duskol.ecdl.utils.DTOToEntityConverter;
 import com.duskol.ecdl.utils.EntityToDTOConverter;
@@ -20,10 +27,17 @@ import com.duskol.ecdl.utils.EntityToDTOConverter;
  *
  */
 @Service
+@Transactional
 public class TestServiceImpl implements TestService {
 	
 	@Autowired
 	TestRepository testRepository;
+	
+	@Autowired
+	QuestionRepository questionRepository;
+	
+	@Autowired
+	AnswerRepository answerRepository;
 	
 	@Autowired
 	DTOToEntityConverter dtoToEntityConverter;
@@ -47,12 +61,12 @@ public class TestServiceImpl implements TestService {
 	@Override
 	public TestDTO getTest(Long id) throws ResourceNotFoundException {
 		
-		Test test = testRepository.getOne(id);
-		if(test==null)
+		Optional<Test> testOpt = testRepository.findById(id);
+		if (!testOpt.isPresent())
 			throw new ResourceNotFoundException("Test id:" + id + " not found!", ErrorCodes.TEST_NOT_FOUND);
 		
 		TestDTO testDTO = new TestDTO();
-		entityToDTOConverter.convert(test, testDTO);
+		entityToDTOConverter.convert(testOpt.get(), testDTO);
 		return testDTO;
 	}
 
@@ -77,26 +91,58 @@ public class TestServiceImpl implements TestService {
 	@Override
 	public void deleteTest(Long id) throws ResourceNotFoundException {
 		
-		Test test = testRepository.getOne(id);
-		if(test == null)
+		Optional<Test> testOpt = testRepository.findById(id);
+
+		if (!testOpt.isPresent())
 			throw new ResourceNotFoundException("Test id:" + id + " not found!", ErrorCodes.TEST_NOT_FOUND);
+		
+		List<Question> questions = questionRepository.findByTestId(id);
+		deleteQuestions(questions);
 		
 		testRepository.deleteById(id);
 	}
 
+	
 	@Override
 	public TestDTO updateTest(TestDTO testDTO) throws ResourceNotFoundException {
 		
-		Test test = testRepository.getOne(testDTO.getId());
-		if(test==null)
+		Optional<Test> testOpt = testRepository.findById(testDTO.getId());
+		if (!testOpt.isPresent())
 			throw new ResourceNotFoundException("Test id:" + testDTO.getId() + " not found!", ErrorCodes.TEST_NOT_FOUND);
 		
+		Test test = testOpt.get();
 		dtoToEntityConverter.convert(testDTO, test);
-		
 		Test updatedTest = testRepository.save(test);
+		
 		TestDTO updatedTestDTO = new TestDTO();
 		entityToDTOConverter.convert(updatedTest, updatedTestDTO);
 		
 		return updatedTestDTO;
+	}
+	
+	/**
+	 * 
+	 * @param questions
+	 */
+	private void deleteQuestions(List<Question> questions) {
+		if(questions != null && questions.size() != 0) {
+			for (Question question : questions) {
+				deleteAnswers(question);
+				questionRepository.deleteById(question.getId());
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * @param question
+	 */
+	private void deleteAnswers(Question question) {
+		List<Answer> answers = answerRepository.findByQuestionId(question.getId());
+		if(answers != null && answers.size() != 0) {
+			for (Answer answer : answers) {
+				answerRepository.deleteById(answer.getId());
+			}
+		}
 	}
 }
