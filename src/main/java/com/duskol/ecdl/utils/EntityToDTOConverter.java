0cd0dec6b36d5c992d7015b7b2afee441c6910f4
duskol <dulleo@gmail.com>
2018-12-30 22:48:00 +0100

    convert question and answer added

--- src/main/java/com/duskol/ecdl/utils/EntityToDTOConverter.java
+++ src/main/java/com/duskol/ecdl/utils/EntityToDTOConverter.java
@@ -2,16 +2,19 @@ package com.duskol.ecdl.utils;
 
 import org.springframework.stereotype.Component;
 
+import com.duskol.ecdl.dto.AnswerDTO;
 import com.duskol.ecdl.dto.CompletedExamDTO;
 import com.duskol.ecdl.dto.ExamAnswerDTO;
 import com.duskol.ecdl.dto.ExamQuestionDTO;
 import com.duskol.ecdl.dto.ExamQuestionStatus;
 import com.duskol.ecdl.dto.ExamTestDTO;
+import com.duskol.ecdl.dto.QuestionDTO;
 import com.duskol.ecdl.dto.TestDTO;
 import com.duskol.ecdl.model.Answer;
 import com.duskol.ecdl.model.CompletedExam;
 import com.duskol.ecdl.model.Question;
 import com.duskol.ecdl.model.Test;
+import com.duskol.ecdl.model.TestStatus;
 
 /**
  * 
@@ -21,21 +24,6 @@ import com.duskol.ecdl.model.Test;
 @Component
 public class EntityToDTOConverter {
 
-	/**
-	 * 
-	 * @param createdTest
-	 * @return
-	 */
-	public TestDTO convert(Test createdTest) {
-		TestDTO t = new TestDTO();
-		t.setId(createdTest.getId());
-		t.setDuration(createdTest.getDuration());
-		t.setIsActive(createdTest.getIsActive());
-		t.setName(createdTest.getName());
-		t.setPassingLimit(createdTest.getPassingLimit());
-		t.setTotalExamQuestions(createdTest.getTotalExamQuestions());
-		return t;
-	}
 
 	/**
 	 * 
@@ -108,9 +96,32 @@ public class EntityToDTOConverter {
 		testDTO.setId(test.getId());
 		testDTO.setDuration(test.getDuration());
 		testDTO.setId(test.getId());
-		testDTO.setIsActive(test.getIsActive());
+		testDTO.setStatus(TestStatus.valueOf(test.getStatus()));
 		testDTO.setName(test.getName());
 		testDTO.setPassingLimit(test.getPassingLimit());
 		testDTO.setTotalExamQuestions(test.getTotalExamQuestions());
+		testDTO.setTotalQuestions(test.getQuestions().size());
+	}
+
+	/**
+	 * 
+	 * @param question
+	 * @param questionDTO
+	 */
+	public void convert(Question question, QuestionDTO questionDTO) {
+		questionDTO.setId(question.getId());
+		questionDTO.setText(question.getText());
+		questionDTO.setType(question.getType());
+	}
+
+	/**
+	 * 
+	 * @param answer
+	 * @param answerDTO
+	 */
+	public void convert(Answer answer, AnswerDTO answerDTO) {
+		answerDTO.setId(answer.getId());
+		answerDTO.setText(answer.getText());
+		answerDTO.setIsCorrect(answer.getIsCorrect());
 	}
 }
