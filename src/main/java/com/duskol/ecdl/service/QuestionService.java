fb7bbd6ad80d994b5bd8f78386a1e5359483c165
duskol <dulleo@gmail.com>
2018-12-30 22:50:13 +0100

    delete, get questions added, delete edit question added

--- src/main/java/com/duskol/ecdl/service/QuestionService.java
+++ src/main/java/com/duskol/ecdl/service/QuestionService.java
@@ -1,12 +1,22 @@
 package com.duskol.ecdl.service;
 
+import java.util.List;
+
 import com.duskol.ecdl.controller.exception.ResourceNotFoundException;
 import com.duskol.ecdl.dto.QuestionDTO;
 import com.duskol.ecdl.model.Question;
 
 public interface QuestionService {
 	
-	QuestionDTO createQuestion(Long id, QuestionDTO questionDto) throws ResourceNotFoundException;
+	void createQuestion(Long id, QuestionDTO questionDto) throws ResourceNotFoundException;
 
-	Question getQuestion(Long id);
+	QuestionDTO getQuestion(Long id) throws ResourceNotFoundException;
+	
+	void deleteQuestions(List<Question> questions);
+
+	List<QuestionDTO> getQuestions(Long testId) throws ResourceNotFoundException;
+
+	void deleteQuestion(Long questionId) throws ResourceNotFoundException;
+
+	void editQuestion(Long questionId, QuestionDTO questionDTO) throws ResourceNotFoundException;
 }
