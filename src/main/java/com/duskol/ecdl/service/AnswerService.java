ef2d38bc366fd81993f84f3df7d22a87241d9050
duskol <dulleo@gmail.com>
2018-12-30 22:44:16 +0100

    delete, get and edit answers added

--- src/main/java/com/duskol/ecdl/service/AnswerService.java
+++ src/main/java/com/duskol/ecdl/service/AnswerService.java
@@ -2,13 +2,18 @@ package com.duskol.ecdl.service;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import com.duskol.ecdl.controller.exception.ResourceNotFoundException;
 import com.duskol.ecdl.dto.AnswerDTO;
+import com.duskol.ecdl.model.Question;
 
 public interface AnswerService {
 
-	void createAnswers(Long id, @Valid List<AnswerDTO> answers) throws ResourceNotFoundException;
+	void createAnswers(Question savedQuestion, List<AnswerDTO> answers) throws ResourceNotFoundException;
+	
+	void deleteAnswers(Long questionId);
+	
+	List<AnswerDTO> getAnswers(Long questionId);
+
+	void editAnswers(Question question, List<AnswerDTO> answers);
 
 }
