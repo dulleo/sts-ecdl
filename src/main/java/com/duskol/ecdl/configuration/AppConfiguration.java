8243e62e4e06650176cdb4615a5edd8801f94781
automation <automation@avm>
2018-12-31 05:04:59 -0700

    @EnableAspectJAutoProxy added

--- src/main/java/com/duskol/ecdl/configuration/AppConfiguration.java
+++ src/main/java/com/duskol/ecdl/configuration/AppConfiguration.java
@@ -2,10 +2,12 @@ package com.duskol.ecdl.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 //@EnableWebMvc
 //@ComponentScan
 //@EnableWebSecurity
