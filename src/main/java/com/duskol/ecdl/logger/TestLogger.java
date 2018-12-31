e54b9fe2f0e578c0c4a8297a95cf7ec5aeec7410
automation <automation@avm>
2018-12-31 03:22:44 -0700

    create test impl

--- src/main/java/com/duskol/ecdl/logger/TestLogger.java
+++ src/main/java/com/duskol/ecdl/logger/TestLogger.java
@@ -1,14 +1,88 @@
 package com.duskol.ecdl.logger;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.hibernate.JDBCException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
+
+import com.duskol.ecdl.error.ErrorCodes;
+import com.duskol.ecdl.exception.DataIntegrityException;
+import com.duskol.ecdl.exception.InternalException;
+import com.duskol.ecdl.exception.NotFoundException;
+import com.duskol.ecdl.exception.ResourceNotFoundException;
 
 @Aspect
 @Component
 public class TestLogger {
 	
 	private static final Logger logger = LoggerFactory.getLogger(TestLogger.class);
-
+	private static final String MESSAGE_FORMAT = "Method {} unsuccessfully finished. ERROR: {} - {}";
+	
+	@Pointcut("execution(void com.duskol.ecdl.controller.TestController.createTest(com.duskol.ecdl.dto.TestDTO) "
+			+ "&& args(testDTO)")
+	public void createTestPointcut(com.duskol.ecdl.dto.TestDTO testDTO) {
+		//
+	}
+	
+	@Around("createTestPointcut(testDTO)")
+	public void updateItems(ProceedingJoinPoint jp, com.duskol.ecdl.dto.TestDTO testDTO) throws Throwable {
+		
+		String methodName = jp.getSignature().getName();
+		String args = Arrays.toString(jp.getArgs()); 
+		
+		try {
+			logger.info("Method {} has started", methodName);
+			//Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			//logger.info("Method " + UPDATE_MERCHANT_ITEMS_METHOD_NAME +  " requested by user: " + authentication.getName() + "-" + authentication.getAuthorities());
+			logger.info("Args: {}",args);
+			jp.proceed();
+			logger.info("Method {} succesfully successfully finished.......", methodName);
+		} catch (ResourceNotFoundException e) {
+			getNotFoundError(e, ErrorCodes.TEST_CAN_NOT_BE_CREATED, methodName);
+		} catch (DataIntegrityViolationException e) {
+			getDataIntegrityViolationError(e, ErrorCodes.TEST_CAN_NOT_BE_CREATED, methodName);
+		} catch (DataAccessException e) {
+			getDataAccessError(e, ErrorCodes.TEST_CAN_NOT_BE_CREATED, methodName);
+		} catch (Exception e) {
+			getInternalError(e, ErrorCodes.TEST_CAN_NOT_BE_CREATED, methodName);
+		}
+	}
+	
+	private void getNotFoundError(Exception e, ErrorCodes errorCodes, String methodName) throws NotFoundException {
+		logger.error(MESSAGE_FORMAT, methodName, e.getMessage(),e);
+		throw new NotFoundException(e.getMessage(), errorCodes);
+	}
+	
+	private void getDataIntegrityViolationError(DataIntegrityViolationException e, ErrorCodes errorCodes, String methodName) throws DataIntegrityException {
+		generateError(e, errorCodes, methodName);    
+	}
+	
+	private void getDataAccessError(DataAccessException e, ErrorCodes errorCodes, String methodName) throws DataIntegrityException {
+		generateError(e, errorCodes, methodName); 
+	}
+	
+	private void generateError(Exception e, ErrorCodes errorCodes, String methodName) throws DataIntegrityException {
+		if(e.getCause() instanceof JDBCException && ((JDBCException)e.getCause()).getSQLException() != null)
+        {                    
+			String message = ((JDBCException)e.getCause()).getSQLException().getMessage();
+			logger.error(MESSAGE_FORMAT,methodName,message,e);
+            throw new DataIntegrityException(message, errorCodes);
+        } else {
+            logger.error(MESSAGE_FORMAT,methodName,e.getMessage(), e);
+            throw new DataIntegrityException(e.getMessage(), errorCodes); 
+        }
+	}
+	
+	private void getInternalError(Exception e, ErrorCodes errorCodes, String methodName) throws InternalException {
+		logger.error(MESSAGE_FORMAT, methodName, e.getMessage(), e);
+		throw new InternalException(e.getMessage(), errorCodes);
+	}
 }
