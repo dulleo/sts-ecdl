307f0df48edbdfbdd32cd682c8459f7c0a9feb2a
automation <automation@avm>
2018-12-31 01:49:09 -0700

    ResourceNotFoundException import

--- src/main/java/com/duskol/ecdl/controller/TestController.java
+++ src/main/java/com/duskol/ecdl/controller/TestController.java
@@ -5,8 +5,6 @@ import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,61 +18,58 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.duskol.ecdl.controller.exception.ResourceNotFoundException;
 import com.duskol.ecdl.dto.TestDTO;
 import com.duskol.ecdl.error.ErrorCodes;
 import com.duskol.ecdl.error.ErrorResponse;
+import com.duskol.ecdl.exception.ResourceNotFoundException;
 import com.duskol.ecdl.service.TestService;
 
 @RestController
 @RequestMapping(value="/ecdl/tests")
 public class TestController {
 	
-	private final static Logger logger = LoggerFactory.getLogger(TestController.class);
-	
 	@Autowired
 	TestService testService;
 	
 	@PostMapping(consumes = "application/json", produces = "application/json")
 	@ResponseStatus(value=HttpStatus.CREATED)
-	public TestDTO createTest(@RequestBody @Valid @NotNull TestDTO testDTO) {
-		logger.info("Save " + testDTO.toString());
-		TestDTO createdTestDTO = testService.createTest(testDTO);
-		logger.info("Saved " + createdTestDTO.toString());
-		return createdTestDTO;
+	public void createTest(@RequestBody @Valid @NotNull TestDTO testDTO) {
+		//logger.info("Save {}", testDTO.toString());
+		testService.createTest(testDTO);
+		//logger.info("Saved " + createdTestDTO.toString());
 	}
 	
 	@PutMapping(consumes = "application/json", produces = "application/json")
 	@ResponseStatus(value=HttpStatus.NO_CONTENT)
 	public TestDTO editTest(@RequestBody @Valid @NotNull TestDTO testDTO) throws ResourceNotFoundException {
-		logger.info("Update testDTO: " + testDTO.toString());
+		//logger.info("Update testDTO: " + testDTO.toString());
 		TestDTO updatedTestDTO = testService.editTest(testDTO);
-		logger.info("Updated testDTO: " + testDTO.toString());
+		//logger.info("Updated testDTO: " + testDTO.toString());
 		return updatedTestDTO;
 	}
 	
 	@GetMapping("/{id}")
     @ResponseStatus(value = HttpStatus.OK)
     public TestDTO getTest(@PathVariable Long id) throws ResourceNotFoundException {
-        logger.info("Get test with id: " + id);
+        //logger.info("Get test with id: " + id);
         TestDTO testDTO = testService.getTest(id);
-        logger.info(testDTO.toString());
+        //logger.info(testDTO.toString());
         return testDTO;
 	}
 	
 	@GetMapping()
 	@ResponseStatus(value=HttpStatus.OK)
 	public List<TestDTO> getTests() throws ResourceNotFoundException {
-		logger.info("Get tests called....");
+		//logger.info("Get tests called....");
 		List<TestDTO> testDTOs = testService.getTests();
-		logger.info("Tests: " + testDTOs.toString());
+		//logger.info("Tests: " + testDTOs.toString());
 		return testDTOs;
 	}
 	
 	@DeleteMapping("/{id}")
 	@ResponseStatus(value=HttpStatus.NO_CONTENT)
 	public void deleteTest(@PathVariable Long id) throws ResourceNotFoundException {
-		logger.info("Delete test id: " + id);
+		//logger.info("Delete test id: " + id);
 		testService.deleteTest(id);
 	}
 	
