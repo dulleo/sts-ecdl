94b36c5162a2cea94e1db81fed5bcaec19ac7bde
duskol <dulleo@gmail.com>
2019-01-02 07:35:58 +0100

    RequestMapping replace post,delete,put and get mapping

--- src/main/java/com/duskol/ecdl/controller/TestController.java
+++ src/main/java/com/duskol/ecdl/controller/TestController.java
@@ -7,14 +7,11 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,46 +25,41 @@ import com.duskol.ecdl.exception.ResourceNotFoundException;
 import com.duskol.ecdl.service.TestService;
 
 @RestController
-@RequestMapping(value="/ecdl/tests")
+@RequestMapping(value="")
 public class TestController {
 	
 	@Autowired
 	TestService testService;
 	
-	@PostMapping(consumes = "application/json", produces = "application/json")
 	@ResponseStatus(value=HttpStatus.CREATED)
+	@RequestMapping(value="/ecdl/tests", consumes = "application/json", produces = "application/json", method= RequestMethod.POST)
 	public void createTest(@RequestBody @Valid @NotNull TestDTO testDTO) {
 		testService.createTest(testDTO);
 	}
 	
-	@PutMapping(consumes = "application/json", produces = "application/json")
 	@ResponseStatus(value=HttpStatus.NO_CONTENT)
+	@RequestMapping(value="/ecdl/tests", consumes = "application/json", produces = "application/json", method=RequestMethod.PUT)
 	public void editTest(@RequestBody @Valid @NotNull TestDTO testDTO) throws ResourceNotFoundException {
 		testService.editTest(testDTO);
 	}
 	
-	@GetMapping("/{id}")
     @ResponseStatus(value = HttpStatus.OK)
+	@RequestMapping(value="/ecdl/tests/{id}", method=RequestMethod.GET)
     public TestDTO getTest(@PathVariable Long id) throws ResourceNotFoundException {
-        //logger.info("Get test with id: " + id);
         TestDTO testDTO = testService.getTest(id);
-        //logger.info(testDTO.toString());
         return testDTO;
 	}
 	
-	@GetMapping()
 	@ResponseStatus(value=HttpStatus.OK)
+	@RequestMapping(value="/ecdl/tests", method=RequestMethod.GET)
 	public List<TestDTO> getTests() throws ResourceNotFoundException {
-		//logger.info("Get tests called....");
 		List<TestDTO> testDTOs = testService.getTests();
-		//logger.info("Tests: " + testDTOs.toString());
 		return testDTOs;
 	}
 	
-	@DeleteMapping("/{id}")
 	@ResponseStatus(value=HttpStatus.NO_CONTENT)
+	@RequestMapping(value="/ecdl/tests/{id}", method=RequestMethod.DELETE)
 	public void deleteTest(@PathVariable Long id) throws ResourceNotFoundException {
-		//logger.info("Delete test id: " + id);
 		testService.deleteTest(id);
 	}
 	
